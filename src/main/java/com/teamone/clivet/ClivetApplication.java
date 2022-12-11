9f72365fd2e27af03c36930bb54c2431a21c8c94
minvoo <maciejewski_mariusz@wp.pl>
2022-12-11 11:16:55 +0100

    Initial commit

---
+++ src/main/java/com/teamone/clivet/ClivetApplication.java
@@ -0,0 +1,13 @@
+package com.teamone.clivet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClivetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClivetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamone/clivet/ClivetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamone.clivet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClivetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
