53deb5302804f95f0267f4a9c0c496e800e6b5ed
minvoo <maciejewski_mariusz@wp.pl>
2022-12-11 11:17:43 +0100

    Initial commit

--- src/test/java/com/teamone/clivet/ClivetApplicationTests.java
+++ src/test/java/com/teamone/clivet/ClivetApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ClivetApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
